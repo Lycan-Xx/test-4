d97ac07152b69f22116dc8e08b8930bd4265290d
Mustee0ty <Mustaphatasiu55@gmail.com>
2024-10-20 07:40:49 +0100

    Experience Page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react"
+import AISparkle from "../assets/GreenSparkle.svg"
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
+ return (
+  <main className="flex flex-row justify-between align-middle">
+   <form className="w-[38vw]" action="">
+    <header className="flex flex-row justify-between">
+     <h5 className="font-lato font-bold text-2xl whitespace-nowrap">
+      Experience.
+     </h5>
+     <div className="flex flex-row text-primary align-middle items-center">
+      <img className="text-primary" src={AISparkle} alt="" />
+      <p className="whitespace-nowrap  border-b-primary border-b-[1px]">
+       Generate Cover Letter
+      </p>
+     </div>
+    </header>
+    <p className="normal-text">Review your experience history.</p>
+   </form>
+   <section className="border-[1px] border-[#d0d5dd] w-[45vw]"></section>
+  </main>
+ )
 }
 
 export default Experience
